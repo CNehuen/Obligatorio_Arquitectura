4b8c58b52db2125d964ab4f652e571396aa26484
CNehuen <62556659+CNehuen@users.noreply.github.com>
2021-05-26 10:47:38 -0300

    Update dibujarString.asm

--- dibujarString.asm
+++ dibujarString.asm
@@ -11,3 +11,15 @@
 #Salir 
 
 dibujarString:
+	#$a0 -> string para dibujar
+	#$a1 -> coordenada X del String en la pantalla
+	#$a2 -> coordenada Y del String en la pantalla
+	#$a3 -> color?
+	# -----PROLOGO-----#
+	#Guardo $ra en sp para no perderlo
+	addu $t1, $a0,$0
+	li $t0,0  #Contador para recorrer el string
+	loopString:
+		lb $t1, ($a0)
+		
+	
