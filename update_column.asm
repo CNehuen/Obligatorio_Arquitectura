2f746f05dc0af0eadb204ee5eed985a4d7a80fc1
CNehuen <62556659+CNehuen@users.noreply.github.com>
2021-06-07 19:19:19 -0300

    bugfixing update_column

--- update_column.asm
+++ update_column.asm
@@ -12,6 +12,7 @@ update_column:
 	lb $t2, ($t1)
 	lb $t3, 1($t1)
 	li $t4, -1  #variable de seleccion de columna para mover
+	beqz $t3, create_first_columns
 	#Desplazo la hilera de la derecha de las columnas hacia la izquierda de la columna
 	loop_move_columns:
 		addiu $t4, $t4,1
@@ -24,34 +25,86 @@ update_column:
 			lw $t7, ($t8)  #almaceno la info del pixel 
 			sw $zero, ($t8)#limpio el pixel
 			blt $t2, 10, errase_column
-			sub $t6, $t8, 44  #me muevo hacia atras hacia donde debo pintar la columna
+			sub $t8, $t8, 40  #me muevo hacia atras hacia donde debo pintar la columna
 			sw $t7, ($t8)  # pinto el pixel 1 posicion a la izq de donde esta la columna 
-			addiu $t8,$t8, 44 # me muevo a la posicion de la derecha de la columna
+			addiu $t8,$t8, 40 # me muevo a la posicion de la derecha de la columna
 			errase_column:
 			addiu $t8,$t8, 512 # me muevo a la fila de abajo
 			j loop_move_column
 			
 	end_move_columns:
-	beqz $t4, loop_move_columns
-	subi $t3, $t3, 1
 	subi $t2, $t2, 1
+	sb $t2, ($t1)
+	addiu $t1,$t1,1
+	lb $t2, ($t1)
+	beqz $t4, loop_move_columns
+	la $t1, column_coord
+	lb $t2, ($t1)
 	beqz $t2, delete_first_column
 	blt $t2, 10, create_column_right
-	sw $t2, ($t1)
-	sw $t3, 1($t1)
+
 	j end_update_column
 	
 	delete_first_column: #si se borro por completo la columna de la izq, la segunda pasa  a ser la de la izq y la de la derecha pasa a ser la segunda
-	sw $t3, ($t1)
-	li $t2, 127
-	sw $t2, 1($t1)
-	j end_update_column
+		sb $t3, ($t1)
+		li $t2, 127
+		sb $t2, 1($t1)
+		j end_update_column
 		
-	create_column_right:
-	li $t0, 117
-	addu $a0, $t0, $t2
-	jal  create_column
-	j end_update_column
+	create_column_right:  
+		li $t0, 117
+		addu $a0, $t0, $t2
+		jal  create_column
+		j end_update_column
+
+	create_first_columns:
+		beqz $t2, create_first_column
+		li $t4, 0
+		ble $t2, 73, create_second_column
+		j loop_move_columns
+		
+		create_first_column:
+			lb $t5, 2($t1)
+			addi $t6, $t5, 117
+			move $a0, $t6
+			jal  create_column
+			subi $t5,$t5,1
+			la $t1, column_coord
+			beqz $t5, created_first
+			sb $t5, 2($t1)
+			j end_update_column
+			
+			created_first:
+			li $t5, 127
+			sb $t5, ($t1)
+			li $t5, 10
+			sb $t5, 2($t1)
+			j end_update_column
+		
+		create_second_column:
+			lb $t5, 2($t1)
+			addi $t6, $t5, 117
+			move $a0, $t6
+			jal  create_column
+			subi $t5,$t5,1
+			la $t1, column_coord
+			beqz $t5, created_second
+			sb $t5, 2($t1)
+			j loop_move_columns
+			
+			created_second:
+			li $t5, 127
+			sb $t5, 1($t1)
+			li $t5, 10
+			sb $t5, 2($t1)
+			j loop_move_columns
+	
+	set_second_column:
+		li $t3, 127
+		sb $t3, 1($t1)
+		j loop_move_columns
+	
+
 
 	end_update_column:
 	#-----EPILOGO-----#
@@ -69,7 +122,7 @@ create_column:
 	
 	la $t1, column_heigh
 	la $t0, img
-	lb $t9, 3($t1)
+	lb $t9, 2($t1)
 	beqz $t9, get_random_heigh		#altura aleatoria de la columna superior
 	continue_create_colum:
 	li $a1, 0
@@ -80,12 +133,14 @@ create_column:
 	li $t8, 64
 	sub $a2, $t8, $a1
 	jal ColumnasVerticales		#pinto la columna inferior
+	j end_create_column
 	
 	get_random_heigh:
-	#creo la altura aleatoria de la columna superior y la almaceno
-	j continue_create_colum
+		#creo la altura aleatoria de la columna superior y la almaceno
+		li $t9, 18
+		j continue_create_colum
 	
-	
+	end_create_column:
 	#-----EPILOGO-----#
     lw $ra , ($sp) 	
 	addi $sp, $sp, 4 
