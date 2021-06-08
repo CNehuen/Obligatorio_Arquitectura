d4534be90d7641fd0ca93bcf15a7ecb04e229648
CNehuen <62556659+CNehuen@users.noreply.github.com>
2021-06-08 11:18:04 -0300

    agrego alturas variables a update_column

--- main.asm
+++ main.asm
@@ -10,6 +10,7 @@
 .globl font8x8
 .globl column_coord
 .globl column_heigh
+.globl pseudorandom_values
 .data 
 .align 2
 img: .space 32768 #espacio de memoria para la imagen 
@@ -28,6 +29,8 @@ MenuJuego2: .asciiz "RANKING"
 MenuJuego3: .asciiz "SALIR"
 column_coord: .space 3  #coordenadas en x de cada una de las columnas. la 3ra coordenada es un auxiliar
 column_heigh: .space 3 #corrdenadas en y de los espacios de las columnas. la 3ra posicion es para la columna que se crea mientas se borra la de las a  la izq
+pseudorandom_values: .space 13 #13 valores que simulan aleatoriedad y un indice de seleccion:[0]-> indice; [1 -12]->numero 
+
 .eqv keyboard_cmd 0xFFFF0012
 .eqv keyboard_pressed 0xFFFF0014
 .text
