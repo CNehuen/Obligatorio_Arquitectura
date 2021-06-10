a7a1fff7d49eb06674f360f61906ef73dc03051a
AgustinAbo22 <84516778+AgustinAbo22@users.noreply.github.com>
2021-06-10 00:28:03 -0300

    Pajaro

--- update_bird.asm
+++ update_bird.asm
@@ -4,84 +4,67 @@ update_bird:
    			#-----PROLOGO-----#
 			#Guardo $ra en sp para no perderlo
 			addi $sp, $sp, -4 
-			sw $ra, ($sp)
-			
-			la $a0, bird8x8
-			li $a1, 20	
-			li $a2,	20	
-			jal dibujarString 
+			sw $ra, ($sp)		
 			
 			movimientoPajaro: 
 				la $t1, coordenadaenY #Vector que tiene Yactual y alpha
 				lb $t2, ($t1) 			#Posicion actual #Aca tengo el 20
 				lb $t3, 1($t1) 			#Alpha #Aca tengo un 0 #T3 asociado al boton
-				##BOTON##        #Si t3 esta en uno significa que apreto el boton
-				#SI VOS APRETAS EL BOTON $t3 va de dos en dos 
-				############################################
-				li $t8, 0 #CONTADOR 
-				beqz $t3, BajarPajaro   #Imagino que me apretan el boton
-				bgt $t3, 1, SubiendoPajaro  #El boton hace que suba 2 pixeles
+				
+				la $a0, limpiarPajaro #Es para que borre  
+				li $a1, 20 	#Posicion en x siempre es 20
+				move $a2, $t2 #Voy a tener mi posicion en y 
+				jal dibujarString   
+				la $t1, coordenadaenY     				        				
+				lb $t2, ($t1) 
+				lb $t3, 1($t1) 
+				bltz $t3, BajarPajaro   	#Imagino que me apretan el boton
+				bgez $t3, SubiendoPajaro  #El boton hace que suba 2 pixeles
 										
 														
-					SubiendoPajaro: 
-					la $a0, ' ' #Es para que borre  
-					li $a1, 20 	#Posicion en x siempre es 20
-					li $a2,	$t2 #Voy a tener mi posicion en y 
-					jal dibujarString
+					SubiendoPajaro: 					
 					sub $t2, $t2, 1 	#Sube 1 pixel 
 					la $a0, bird8x8     #Dibuja el pajaro
 					li $a1, 20 			#Posicion en x
-					li $a2, $t2			#Nueva posicion en y 19 
+					move $a2, $t2			#Nueva posicion en y 19 
+					  	
 					sub $t3, $t3, 1     #Vuelvo mi boton a 0   
-					sw $t3, 1($t1)  	#Lo guardo					   
-					sw $t2, ($t1)    	#Guardo mi nueva posicion actual 
-					addi $t8, $t8, 1  	#Sumo uno al contador
-					jal dibujarString  	#Dibujo el pajaro 
-					la $t1, coordenadaenY #Leo mis nuevos datos
-					lb $t2, ($t1)         ##LEO NUEVAMENTE
-					lb $t3, 1($t1)      				
-					beq $t3, 1, SeguirSubiendo #Me queda subir un pixel mas 2 mas
-					beq $t3, 0, Subo			#Si t3 es 0 solo me queda subir un pixel
+					sb $t3, 1($t1)  	#Lo guardo					   
+					sb $t2, ($t1)    	#Guardo mi nueva posicion actual 
+					jal dibujarString		#Dibujo el pajaro
+					move $v0, $zero  			  						 
+					j finMovimientoPajaro  
 					
-					Subo:
-					bne $t8, 2, SubiendoPajaro   
-					j movimientoPajaro
-					
-					SeguirSubiendo:
-					bne $t8, 4, SubiendoPajaro
-					j movimientoPajaro
 									
-					BajarPajaro:
-					la $a0, " " #Es para que borre  
-					li $a1, 20 	#Posicion en x siempre es 20
-					li $a2,	$t2 #Voy a tener mi posicion en y
-					jal dibujarString
-					addi $t2, $t2, 1					 #Baja un pixel  
+					BajarPajaro:				
+					addi $t2, $t2, 1		 #Baja un pixel
+					beq $t2, 54, partidaPerdida  
 					la $a0, bird8x8
 					li $a1, 20 
-					li $a2, $t2  
-					sub $t2, $t2, 1 
-					sw $t2, ($t1)
-					addi $t8, $t8, 1 
-					ble  $t8, 1, Pregunto
-					j movimientoPajaro
+					move $a2, $t2 				
+					sub $t3, $t3, 1
+					sb $t3, 1($t1)					 
+					sb $t2, ($t1)							
+					jal dibujarString		#Dibujo el pajaro
+					move $v0, $zero  
 					
-					Pregunto:
-					beqz  $t3, BajarPajaro
-					j Subo
+					j finMovimientoPajaro
 					
-			fin:
+				
+			
+			partidaPerdida: 	
+					li $v0, 1
 					
-					
+						
+			finMovimientoPajaro:		
+								
+															
+						
 			#-----EPILOGO-----#
     		lw $ra , ($sp) 	
 			addi $sp, $sp, 4 
-	
-				
-						
-			jr $ra 	
-			
-			
+							
+			jr $ra	
 			
 			
 			##El pajaro va arrancar teniendo 
